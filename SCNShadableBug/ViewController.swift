b706f845c2700e91a79f5cb980b0e13c629625c4
Guillaume Sabran <sabranguillaume@gmail.com>
2016-08-14 18:51:21 -0700

    Initial Commit

---
+++ SCNShadableBug/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SCNShadableBug
+//
+//  Created by Guillaume Sabran on 8/14/16.
+//  Copyright © 2016 Guillaume Sabran. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
